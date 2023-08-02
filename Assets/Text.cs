8e81f46cb7fb4cf9098ac78fe92d464a58c88a84
whitedabaic <1951893824@qq.com>
2023-08-02 18:38:03 +0800

    第三次提交

--- Assets/Text.cs
+++ Assets/Text.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Text : MonoBehaviour
 {
-    private string str = "First";
+    private string str = "third";
     // Start is called before the first frame update
     void Start()
     {
